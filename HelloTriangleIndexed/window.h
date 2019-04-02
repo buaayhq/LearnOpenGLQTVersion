26b38d8eab284f0647d6eed14dfd3c01a8be616d
岳会强 <yuehuiqiang@zoezen.cn>
2019-04-02 18:18:39 +0800

    初始化版本

---
+++ HelloTriangleIndexed/window.h
@@ -0,0 +1,36 @@
+﻿#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <QOpenGLWindow>
+#include <QOpenGLFunctions>
+#include <QOpenGLBuffer>
+#include <QOpenGLVertexArrayObject>
+
+class QOpenGLShaderProgram;
+
+class Window : public QOpenGLWindow,
+               protected QOpenGLFunctions
+{
+  Q_OBJECT
+
+public:
+    Window();
+    ~Window();
+// OpenGL Events
+public:
+  void initializeGL();
+  void resizeGL(int width, int height);
+  void paintGL();
+protected slots:
+  void teardownGL(); 
+
+private:
+  // OpenGL State Information
+  QOpenGLBuffer *pVBO;
+  QOpenGLBuffer *pEBO;
+  QOpenGLVertexArrayObject *pVAO;
+  QOpenGLShaderProgram *pShaderProgram;
+
+};
+
+#endif // WINDOW_H
